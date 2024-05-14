4a69d47944f17038f8347f9f6d3adf431b3259a4
Nick Simmons <nicksimmons53@gmail.com>
2024-05-14 17:36:54 -0500

    SharePoint folder creation

--- routes/sharepoint.js
+++ routes/sharepoint.js
@@ -106,16 +106,14 @@ router.post('/folder', async function(req, res, next) {
     "@microsoft.graph.conflictBehavior": "rename",
   }
 
-  console.log(body);
+  let axiosRes = await axios.post(`${graphAPI}/${process.env.SITE_ID}/drive/items/${parentId}/children`, body, {
+    headers: {
+      "Authorization": `Bearer ${tokenRes.access_token}`,
+      "Content-Type": "application/json",
+    }
+  });
 
-  // let axiosRes = await axios.post(`${graphAPI}/${process.env.SITE_ID}/drive/items/${parentId}/children`, body, {
-  //   headers: {
-  //     "Authorization": `Bearer ${tokenRes.access_token}`,
-  //     "Content-Type": "application/json",
-  //   }
-  // });
-  res.send(body)
-  // res.send(axiosRes.data);
+  res.send(axiosRes.data);
 });
 
 module.exports = router;
